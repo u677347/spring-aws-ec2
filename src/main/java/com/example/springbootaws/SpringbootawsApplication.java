4cd47eafc1cd91f5f82a44683d7c2f8711041e4b
Sudhir Mukkamalla <sudhir.mukkamalla@gmail.com>
2023-06-10 12:31:12 +0530

    first commit

---
+++ src/main/java/com/example/springbootaws/SpringbootawsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootawsApplication.class, args);
+	}
+
+}
